143a1bec1ab3e18856f369ec38b0adbabfb4ee27
Rocy-June <1048654629@qq.com>
2025-01-22 17:40:42 +0900

    fix: add new match pattern

--- rlcnf.net/bbs/board.php-bo_table=information_processi/script.js
+++ rlcnf.net/bbs/board.php-bo_table=information_processi/script.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         기출넷 Customize
 // @namespace    http://tampermonkey.net/
-// @version      25.01.15.01
+// @version      25.01.22.01
 // @description  함께 성장하는 기출넷, 당신의 성공을 응원합니다!
 // @author       Rocy
 // @match        https://rlcnf.net/bbs/board.php?bo_table=information_processi*
+// @match        https://rlcnf.net/bbs/board.php?bo_table=info_process_p*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=rlcnf.net
 // @grant        none
 // ==/UserScript==
